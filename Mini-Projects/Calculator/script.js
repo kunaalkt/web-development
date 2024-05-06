276617ee4610c0592b8dfdac337540f2328ca163
Kunaal <kunaalkt26@gmail.com>
2024-05-06 22:56:24 +0530

    Implemented onclick events in js file

--- Mini-Projects/Calculator/script.js
+++ Mini-Projects/Calculator/script.js
@@ -1,16 +1,46 @@
-let calculate = '';
-const pElement = document.querySelector('.result-text');
+let calculate = "";
+const pElement = document.querySelector(".js-result-text");
+
+const operandButtonElement = document.querySelectorAll(".js-operand-btn");
+operandButtonElement.forEach((button) => {
+  button.addEventListener("click", () => {
+    updateExpression(button.innerText);
+  });
+});
+
+const operatorButtonElement = document.querySelectorAll(".js-operator-btn");
+operatorButtonElement.forEach((button) => {
+  button.addEventListener("click", () => {
+    updateExpression(button.innerText);
+  });
+});
+
+const calculateButtonElement = document.querySelector(".js-calculate-btn");
+calculateButtonElement.addEventListener("click", calculateExpression);
+
+const clearButtonElement = document.querySelector(".js-clr-btn");
+clearButtonElement.addEventListener("click", clearExpression);
+
 function updateExpression(operator) {
-    calculate += operator;
-    pElement.innerText = calculate;
+  calculate += operator;
+  pElement.innerText = calculate;
 }
 
 function calculateExpression() {
+  if (calculate) {
     calculate = eval(calculate);
+    if (calculate.toString().length > 15) {
+      pElement.style.fontSize = "35px";
+      pElement.innerText = calculate;
+    } else if (calculate.toString().length > 30) {
+      pElement.style.fontSize = "25px";
+      pElement.innerText = calculate;
+    }
     pElement.innerText = calculate;
+  }
 }
 
 function clearExpression() {
-    calculate = '';
-    pElement.innerText = calculate;
+  calculate = "";
+  pElement.innerText = calculate;
 }
